a0672f74f45c59e4bef4ab6d74038300105d2589
root <matheusfjost@outlook.com>
2024-05-16 21:37:37 -0300

    feat: commit inicial
    definido padrao mvc

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# orbitguard
+# ☄️  ORBIT GUARD
+
+## App desktop baseado em Java Swing para monitorar e analisar asteroides e cometas que passam perto da Terra.
+
+...
---
+++ orbitguard/src/main/java/com/orbitguard/App.java
@@ -0,0 +1,7 @@
+package com.orbitguard;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("start of the world...");
+    }
+}
