2b2a213bc89bfc07b36a6537818d5737bc02237d
Gustavo Becker <gustavobe0504@gmail.com>
2024-06-16 21:10:46 -0300

    feat(View): Adicionado MenuDrawer

--- src/main/java/com/orbitguard/orbitguard/OrbitguardApplication.java
+++ src/main/java/com/orbitguard/orbitguard/OrbitguardApplication.java
@@ -1,18 +1,18 @@
 package com.orbitguard.orbitguard;
 
+
+import com.orbitguard.orbitguard.view.home.Home;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.orbitguard.orbitguard.view.home.HomeScreen;
-
 @SpringBootApplication
 public class OrbitguardApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(OrbitguardApplication.class)
-				.headless(false).run(args);
-		ctx.getBean(HomeScreen.class);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(OrbitguardApplication.class)
+                .headless(false).run(args);
+        ctx.getBean(Home.class);
+    }
 
 }
---
+++ src/main/java/com/orbitguard/orbitguard/view/menu/MenuEvent.java
@@ -0,0 +1,10 @@
+package com.orbitguard.orbitguard.view.menu;
+
+/**
+ *
+ * @author RAVEN
+ */
+public interface MenuEvent {
+
+    public void selected(int index, int subIndex);
+}
